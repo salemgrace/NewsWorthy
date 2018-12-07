dd28e6d98da82dc584273f534f50ae09bf7f8696
salemgrace <salem.bryant@gmail.com>
2018-12-06 22:19:26 -0500

    set up

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+// Routes
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
