ce687588abfb4afd16b35963e67b373a41900ed8
salemgrace <salem.bryant@gmail.com>
2018-12-08 12:28:51 -0500

    updated file setup

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
 var express = require("express");
-var logger = require("morgan");
 var mongoose = require("mongoose");
-
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
+
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
-// Initialize Express
 var app = express();
 
-// Configure middleware
 
-// Use morgan logger for logging requests
-app.use(logger("dev"));
 // Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
+
+
 // Make public a static folder
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+// Handlebars
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+
+// Mongo -- Mongoose
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
 
 // Routes
-
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App running on port " + PORT + "!");
-  });
+});
